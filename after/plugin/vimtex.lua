ad63be35973a0d03c9beef9ff044889622a6ea17
cwrneiro <gutoohk@gmail.com>
2024-09-22 21:50:12 -0300

    changed default build files directory for vimtex

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,2 +1,8 @@
 vim.g.vimtex_view_method = 'zathura'
+
 vim.g.vimtex_quickfix_open_on_warning = 0
+
+vim.g.vimtex_compiler_latexmk = {
+  aux_dir = 'latex_build',  -- Nome do diretório onde os arquivos auxiliares serão armazenados
+}
+
