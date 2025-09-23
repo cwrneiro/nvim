80337dd3e7de798cbf76ebd8436637ba6efe6586
Augusto Carneiro <augusto.carneiro@databricks.com>
2025-09-23 13:32:24 -0300

    added harperls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,6 +18,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+require('lspconfig').harper_ls.setup {}
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {'rust_analyzer', 'pyright'},
