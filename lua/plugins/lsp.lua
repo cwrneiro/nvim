3b9e8a1cd2bf9fd9fb5478148f6c8eefe9e6ae00
Augusto Carneiro <augusto.carneiro@databricks.com>
2026-01-05 18:01:21 -0300

    added autopairs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -107,4 +107,24 @@ return {
       require('luasnip.loaders.from_vscode').lazy_load()
     end,
   },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    config = function()
+      local autopairs = require('nvim-autopairs')
+      autopairs.setup({
+        check_ts = true, -- enable treesitter
+        ts_config = {
+          lua = { 'string' }, -- don't add pairs in lua string treesitter nodes
+          javascript = { 'template_string' },
+          java = false, -- don't check treesitter on java
+        },
+      })
+
+      -- Integration with nvim-cmp
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+    end,
+  },
 }
